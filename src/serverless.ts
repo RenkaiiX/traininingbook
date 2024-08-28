55e2d72b14b9fb02143aa3c5884d2f5498ec2129
Renkaiix <darmaputra1136@GMAIL.com>
2024-08-28 11:33:52 +0800

    second commit

---
+++ src/serverless.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+import serverlessExpress from '@vendia/serverless-express';
+import * as express from 'express';
+
+const expressApp = express();
+
+const createNestServer = async (expressInstance) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+  await app.init();
+};
+
+createNestServer(expressApp);
+
+export const handler = serverlessExpress({ app: expressApp });
